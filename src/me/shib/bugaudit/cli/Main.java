7e51e5031bfdb9d990685198a571ac55c15d6180
Shibly Meeran <git@shib.me>
2019-03-08 20:15:28 +0530

    adding exception signature

--- src/me/shib/bugaudit/cli/Main.java
+++ src/me/shib/bugaudit/cli/Main.java
@@ -4,7 +4,7 @@ import me.shib.bugaudit.core.BugAudit;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         if (BugAudit.audit().size() > 0) {
             System.exit(1);
         }
