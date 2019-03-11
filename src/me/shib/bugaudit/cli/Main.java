fb2c6c1eaa759a477db47b6dc6dc44e93e75c6a2
Shibly Meeran <git@shib.me>
2019-03-11 16:14:25 +0530

    updating to the restructured framework

--- src/me/shib/bugaudit/cli/Main.java
+++ src/me/shib/bugaudit/cli/Main.java
@@ -1,14 +1,11 @@
 package me.shib.bugaudit.cli;
 
-import me.shib.bugaudit.core.BugAudit;
+import me.shib.bugaudit.BugAudit;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         if (BugAudit.audit().size() > 0) {
             System.exit(1);
         }
     }
-
-
 }
