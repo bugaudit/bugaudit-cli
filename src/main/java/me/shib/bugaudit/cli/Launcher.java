02bb3485e125284fbac8263b4a3c4e78ebe79c27
Shibly Meeran <git@shib.me>
2019-07-02 11:58:44 +0530

    updating to fetch the bundled jar directly from releases

--- src/main/java/me/shib/bugaudit/cli/Launcher.java
+++ src/main/java/me/shib/bugaudit/cli/Launcher.java
@@ -4,7 +4,7 @@ import me.shib.bugaudit.BugAudit;
 
 public final class Launcher {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         if (BugAudit.audit().size() > 0) {
             System.exit(1);
         }
